02c5a7ecb8ec5f8ea41cac9dc041cbbdf1e89a8f
TeeVeeoAI <179211594+TeeVeeoAI@users.noreply.github.com>
2025-05-16 10:54:34 +0200

    ++

--- obj/Debug/net8.0-windows/MG4G.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MG4G.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MG4G")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39292219c86bf8adffde87f3530c86bf11606a07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bc1a0266a66f9bde5ece43afc39ce8da1a41a98")]
 [assembly: System.Reflection.AssemblyProductAttribute("MG4G")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MG4G")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
